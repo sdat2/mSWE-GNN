534bc204c409708a4685b473875c25ec65780625
Simon D.A. Thomas <sdat2@cam.ac.uk>
2025-11-10 17:18:42 +0000

    upload

--- upload.py
+++ upload.py
@@ -1,9 +1,11 @@
 import os
 from huggingface_hub import HfApi
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
-api.upload_large_folder(
-    folder_path="../swegnn_5sec/",
-    repo_id="sdat2/surgenet-train",
-    repo_type="dataset",
-)
+if __name__ == "__main__":
+    # python -m upload
+    api = HfApi(token=os.getenv("HF_TOKEN"))
+    api.upload_large_folder(
+        folder_path="../swegnn_5sec/",
+        repo_id="sdat2/surgenet-train",
+        repo_type="dataset",
+    )   
